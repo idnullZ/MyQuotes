12dffdbab96c708fa5a674a990d87c63934a5e42
idnullZ <idnullznz@gmail.com>
2021-12-11 15:08:21 +0600

    Initial commit

---
+++ app/src/main/java/idnull/z/myquotes/MainActivity.kt
@@ -0,0 +1,11 @@
+package idnull.z.myquotes
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
